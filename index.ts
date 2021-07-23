bce112f41cb08f5f7f0197f213cbd375e21d0ab3
Luiz Gustavo Setten <consultor.119@7comm.com.br>
2021-07-22 22:47:13 -0300

    feat: code verify working

--- index.ts
+++ index.ts
@@ -1,10 +1,54 @@
 import express from 'express';
-// rest of the code remains same
+import speakeasy from 'speakeasy';
+import qrcode from 'qrcode';
+import bodyParser from 'body-parser';
+
 const app = express();
 
-const PORT = 8000;
+const PORT = 3333;
 
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/get_secret', (req, res) => {
+  const secret = speakeasy.generateSecret({
+    name: "2FA sample"
+  });
+
+  res.send({ secret });
+}
+);
+
+app.post('/generate_qr_code', (req, res) => {
+  qrcode.toDataURL(String(req.body.secret.otpauth_url), (err, data_url) => {
+    res.send({
+      qr_code: data_url
+    });
+  });
+});
+
+app.post('/verify_token', (req, res) => {
+  // const token2 = speakeasy.totp({
+  //   secret: req.body.secret,
+  //   encoding: 'ascii'
+  // });
+  // console.log("Token valido", token2);
+
+
+  const token_is_valid = speakeasy.totp.verify({
+    secret: req.body.secret,
+    encoding: 'ascii',
+    token: String(req.body.token)
+  });
+
+
+  res.send({ token_is_valid });
+});
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
